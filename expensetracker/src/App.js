efc2874fbccc527c9eac275362f7fe5c7a4ba73d
Deena-toobler <deena.poulose@zuseapps.co.uk>
2025-04-05 13:26:14 +0530

    tst

---
+++ expensetracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseChart from './expenseChart';
+
+function App() {
+  return (
+    <div className="App">
+     <ExpenseChart/>
+    </div>
+  );
+}
+
+export default App;
